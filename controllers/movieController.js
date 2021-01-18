a10db20dc8e55292df9912700e95dd5ef63b56fe
Natasha <natu.rios@hotmail.com>
2021-01-18 18:11:14 -0300

    probando si funciona el token

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -9,12 +9,19 @@ class MovieController{
     let offset = 0;
     let limit = 10;
     
+    
+    const [, token] = req.headers.cookie.split('=');
+    
+    const infoUser = {
+      token: token,
+      username: req.user.name
+    }
     if(req.user){
       if(page){
         try{
           offset = 3 * (page - 1);
           const movie = await this.movieService.getMovies(offset, limit);
-          res.status(200).json(movie);
+          res.status(200).json({...infoUser, movie});
         }catch(e){
           console.log(e);
           res.status(500).send('Error receiving');
@@ -22,7 +29,7 @@ class MovieController{
       }else{
         try{
           const movie = await this.movieService.getMovies();
-          res.status(200).json(movie);
+          res.status(200).json({...infoUser, movie});
         }catch(e){
           console.log(e);
           res.status(500).send('Error receiving');
